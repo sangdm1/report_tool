cfbcf08e8692a15cfcd849baa2596abe60773bd4
Nguyễn Minh Tín <tinnm@runsystem.net>
2023-01-18 14:21:17 +0700

    add roles, edit migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 return new class extends Migration
 {
@@ -15,13 +17,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->enum('role', config('constants.user_roles'));
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('dispaly_name')->nullable();
+            $table->string('code')->unique()->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('google_id')->nullable();
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert(['role' => '1', 'name' => 'Admin', 'dispaly_name' => 'Admin', 'password'=> Hash::make('12345')]);
+
     }
 
     /**
