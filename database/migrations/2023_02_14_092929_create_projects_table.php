35cb40c75c8acb81c9eb5f93f11d79c0929d6043
Nguyễn Minh Tín <tinnm@runsystem.net>
2023-02-16 13:21:28 +0700

    edit migration

--- database/migrations/2023_02_14_092929_create_projects_table.php
+++ database/migrations/2023_02_14_092929_create_projects_table.php
@@ -17,9 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->timestamp('start_at')->nullable();
-            $table->timestamp("created_at")->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp("updated_at")->default(DB::raw('CURRENT_TIMESTAMP'));
-            
+            $table->timestamp('end_at')->nullable();
+            $table->timestamps();
             $table->softDeletesTz($column = 'deleted_at', $precision = 0);
         });
     }
--- database/migrations/2023_02_15_043209_create_members_table.php
+++ database/migrations/2023_02_15_043209_create_members_table.php
@@ -17,8 +17,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger("user_id");
             $table->unsignedBigInteger("project_id");
-            $table->timestamp("created_at")->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp("updated_at")->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp();
             $table->softDeletesTz($column = 'deleted_at', $precision = 0);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
