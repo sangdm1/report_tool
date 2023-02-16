7a62db9a2b226ce5ece073ec040f493dbb403e43
Nguyễn Minh Tín <tinnm@runsystem.net>
2023-02-16 13:34:16 +0700

    edit migration

--- database/migrations/2023_02_15_043209_create_members_table.php
+++ database/migrations/2023_02_15_043209_create_members_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger("user_id");
             $table->unsignedBigInteger("project_id");
-            $table->timestamp();
+            $table->timestamps();
             $table->softDeletesTz($column = 'deleted_at', $precision = 0);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
